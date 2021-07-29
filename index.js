4c94fe1cd67391224c0f3875f8848d6db226a720
Keikishim2 <Keikishim2@gmail.com>
2021-07-29 15:09:51 +0800

    Changes

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
 $(document).ready(function(){
-    $(".step").click( function() {
-	$(this).addClass("active").prevAll().addClass("active");
-	$(this).nextAll().removeClass("active");
-});
-    $(".step01").click( function() {
-    $("#line-progress").css("width", "3%");
-    $(".discovery").addClass("active").siblings().removeClass("active");
-    });
+    $("body")
+        .on("click", ".step",  step1)
+        .on("click", ".step01", step2)
+        .on("click", ".step02", step3)
+        .on("click", ".step03", step4)
+        .on("click", ".step04", step5)
+        .on("click", ".step05", step6)
+        .on("click", "img", drag);
 
-    $(".step02").click( function() {
-    $("#line-progress").css("width", "25%");
-    $(".strategy").addClass("active").siblings().removeClass("active");
-    });
-
-    $(".step03").click( function() {
-    $("#line-progress").css("width", "50%");
-    $(".creative").addClass("active").siblings().removeClass("active");
-    });
-
-    $(".step04").click( function() {
-    $("#line-progress").css("width", "75%");
-    $(".production").addClass("active").siblings().removeClass("active");
-    });
-
-    $(".step05").click( function() {
-	$("#line-progress").css("width", "100%");
-	$(".analysis").addClass("active").siblings().removeClass("active");
-});
-});   
+    function step1(){
+        $(this).addClass("active").prevAll().addClass("active");
+        $(this).nextAll().removeClass("active");
+    }
+    function step2(){
+        $("#line-progress").css("width", "3%");
+        $(".discovery").addClass("active").siblings().removeClass("active");
+    }
+    function step3(){
+        $("#line-progress").css("width", "25%");
+        $(".strategy").addClass("active").siblings().removeClass("active");
+    }
+    function step4(){
+        $("#line-progress").css("width", "50%");
+        $(".creative").addClass("active").siblings().removeClass("active");
+    }
+    function step5(){
+        $("#line-progress").css("width", "75%");
+        $(".production").addClass("active").siblings().removeClass("active");
+    }
+    function step6(){
+        $("#line-progress").css("width", "100%");
+        $(".analysis").addClass("active").siblings().removeClass("active");
+    }
+    function drag(){
+        $("img").draggable();
+    }
+}); 
